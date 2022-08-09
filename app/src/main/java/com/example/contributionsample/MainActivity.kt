6e0f1e24e459848e5c91002a0129b385fa53c55c
Ajin Krishna K <ajin@meridian.net.in>
2022-08-09 12:38:56 +0530

    Added new activity_main2.xml

--- app/src/main/java/com/example/contributionsample/MainActivity.kt
+++ app/src/main/java/com/example/contributionsample/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.contributionsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.contributionsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    fun init() {
+        binding.btnLogin.setOnClickListener {
+            Intent(this, MainActivity2::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
